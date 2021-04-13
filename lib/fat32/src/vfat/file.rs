a056bd309681dcec45aba00fd357e69f27d81b80
Dhole <dhole@riseup.net>
2021-04-13 19:06:18 +0200

    Optimize File::read

--- lib/fat32/src/vfat/file.rs
+++ lib/fat32/src/vfat/file.rs
@@ -10,7 +10,9 @@ use crate::vfat::{Chain, Cluster, Metadata, VFatHandle};
 #[derive(Debug)]
 pub struct File<HANDLE: VFatHandle> {
     pub vfat: HANDLE,
+    cluster_size: u64,
     pub first_cluster: Cluster,
+    pub current_cluster: Cluster,
     // pub chain: Chain<HANDLE>,
     pub size: u32,
     pub pos: u64,
@@ -19,9 +21,12 @@ pub struct File<HANDLE: VFatHandle> {
 impl<HANDLE: VFatHandle> File<HANDLE> {
     pub fn new(vfat: HANDLE, first_cluster: Cluster, size: u32) -> File<HANDLE> {
         // let chain = vfat.chain(first_cluster);
+        let cluster_size = vfat.lock(|vfat| vfat.cluster_size());
         File {
             vfat,
+            cluster_size,
             first_cluster,
+            current_cluster: first_cluster,
             // chain,
             size,
             pos: 0,
@@ -67,6 +72,15 @@ impl<HANDLE: VFatHandle> io::Seek for File<HANDLE> {
             return ioerr!(InvalidInput, "seek outside file");
         };
         self.pos = pos as u64;
+
+        let cluster_index = self.pos / self.cluster_size;
+        let cluster_offset = self.pos % self.cluster_size;
+        let mut chain = self.vfat.chain(self.first_cluster);
+        self.current_cluster = chain
+            .skip(cluster_index as usize)
+            .nth(0)
+            .ok_or(newioerr!(InvalidInput, "position past the end of file"))??;
+
         Ok(self.pos)
     }
 }
@@ -78,39 +92,36 @@ impl<HANDLE: VFatHandle> io::Read for File<HANDLE> {
         if self.pos == self.size as u64 {
             return Ok(0);
         } else if self.pos >= self.size as u64 {
-            return Ok(0);
-            // return ioerr!(InvalidInput, "read past the end of file");
+            return ioerr!(InvalidInput, "read past the end of file");
         }
-        let mut chain = self.vfat.chain(self.first_cluster);
-        let cluster_size = self.vfat.lock(|vfat| vfat.cluster_size());
-        let cluster_index = self.pos / cluster_size;
-        let cluster_offset = self.pos % cluster_size;
-        // for i in 0..cluster_index {
-        //     chain
-        //         .next()
-        //         .ok_or(newioerr!(InvalidInput, "position past the end of file"))?;
-        // }
-        // let cluster = chain.next().ok_or(newioerr!(InvalidInput, "position past the end of file"))?;
-        let cluster = chain
-            .skip(cluster_index as usize)
-            .nth(0)
-            .ok_or(newioerr!(InvalidInput, "position past the end of file"))??;
+        let cluster_index = self.pos / self.cluster_size;
+        let cluster_offset = self.pos % self.cluster_size;
 
-        let mut cluster_data = vec![0; cluster_size as usize];
+        let cluster_data_len = if cluster_index == self.size as u64 / self.cluster_size {
+            self.size as u64 % self.cluster_size
+        } else {
+            self.cluster_size
+        };
+        let mut cluster_data = vec![0; cluster_data_len as usize];
         self.vfat.lock(|vfat| -> io::Result<()> {
-            // vfat.read_chain(self.first_cluster, &mut file_buf)?;
-            vfat.read_cluster(cluster, &mut cluster_data)?;
+            vfat.read_cluster(self.current_cluster, &mut cluster_data)?;
             Ok(())
         });
-        let cluster_data_len = if cluster_index == self.size as u64 / cluster_size {
-            self.size as u64 % cluster_size
-        } else {
-            cluster_size
-        };
         let len = core::cmp::min(buf.len() as u64, cluster_data_len - cluster_offset) as usize;
         buf[..len]
             .copy_from_slice(&cluster_data[cluster_offset as usize..cluster_offset as usize + len]);
         self.pos += len as u64;
+
+        // If we just read until the end of the cluster, and it's not the last cluster, preload the
+        // next cluster index
+        if self.pos != self.size as u64
+            && cluster_offset as usize + len == cluster_data_len as usize
+        {
+            let mut chain = self.vfat.chain(self.current_cluster);
+            self.current_cluster = chain
+                .nth(1)
+                .ok_or(newioerr!(InvalidInput, "position past the end of file"))??;
+        }
         Ok(len)
     }
 }
